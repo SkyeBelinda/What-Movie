c324040356191a84e13fb0f741c8306d1a73b61e
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-10-13 12:12:04 +1300

    api requesting

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 
-const movies = require('./routes/movies')
+// const movies = require('./routes/movies')
 
 const server = express()
 server.use(express.static('public'))
 
-server.use('/api/v1/movies', movies)
+server.use('/api/v1/movies', require('./routes'))
 
 module.exports = server
