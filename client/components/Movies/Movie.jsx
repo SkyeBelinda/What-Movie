156f279bf50fe6cf01efe7b8d3547bc05d5d1262
jon-atto-bennett <jon.bennett@outlook.co.nz>
2017-10-13 15:06:30 +1300

    single linked with API data

--- client/components/Movies/Movie.jsx
+++ client/components/Movies/Movie.jsx
@@ -1,34 +1,48 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {getMovieData} from '../../client-api'
 
-function Movie (props) {
-  const {id, movieName, otherName} = props.movie
-  return (
-    <div className="">
+class Movie extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      data: null
+    }
+  }
+  componentDidMount() {
+    this.getSingleMovieData(this.props.movie.title)
+  }
+  componentWillReceiveProps(props) {
+    this.getSingleMovieData(props.movie.title)
+  }
+  getSingleMovieData(title) {
+    if (title) getMovieData(title)
+      .then(data => this.setState({data}) )
+  }
+  render () {
+    console.log(this.state)
+    const {data} = this.state
+    const {id, title, plot} = this.props.movie
+    return (
       <div className="">
-        <div className="">
-          <div className="">
-            <div className="">
-              <table className="">
-                <thead>
-                  <tr>
-                    <th>Movie Name</th>
-                    <th>{movieName}</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>Other Name</td>
-                    <td>{otherName}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        <h1>{title}</h1>
+        {/* <h3>{this.state.data}</h3> */}
+        {data && <div>
+          <h3>{data.overview}</h3>
+          <img src={`http://image.tmdb.org/t/p/w185/${data.backdrop_path}`} />
+          <img src={`http://image.tmdb.org/t/p/w185/${data.poster_path}`} />
+        </div>}
       </div>
-    </div>
-  )
+    )
+  }
+}
+
+Movie.defaultProps = {
+  movie: {
+    id: 1,
+    title: null,
+    plot: 'Jeff'
+  }
 }
 
 export default Movie
